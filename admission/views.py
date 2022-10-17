3196804b297c904930521aa88d1a88e066f15c8a
KeepLearningEvenImStupid <tianivan238@gmail.com>
2022-10-17 18:48:11 +0700

    Aku edit sedikit

--- admission/views.py
+++ admission/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from admission.models import Admission
 from rest_framework import viewsets
 from .serializer import AdmissionSerializer
+from django.contrib.auth.decorators import login_required
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class AdmissionViewset(viewsets.ModelViewSet):
     queryset = Admission.objects.all()
     serializer_class = AdmissionSerializer
+    permission_classes = [IsAuthenticated]
