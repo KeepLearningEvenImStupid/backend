0f41aff00147cad87c122a05b67014670d12fb47
KeepLearningEvenImStupid <tianivan238@gmail.com>
2022-10-17 16:44:48 +0700

    Push UserManagement

--- admission/views.py
+++ admission/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from admission.models import Admission, AdmissionSerializer
+from admission.models import Admission
 from rest_framework import viewsets
-
+from .serializer import AdmissionSerializer
 # Create your views here.
+
+
 class AdmissionViewset(viewsets.ModelViewSet):
     queryset = Admission.objects.all()
     serializer_class = AdmissionSerializer
--- project/urls.py
+++ project/urls.py
@@ -3,16 +3,16 @@ from user.views import *
 from user import views
 from django.contrib import admin
 from django.urls import path, include
-from admission.views import AdmissionViewset
+from admission.views import *
 from rest_framework import routers
 
-
 router = routers.DefaultRouter()
-router.register('admission/', AdmissionViewset)
+
+router.register('admission', AdmissionViewset)
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('User-Register/', userRegisterAPI.as_view(), name='register'),
     path('Dosen-Register/', dosenRegisterAPI.as_view(), name='register'),
