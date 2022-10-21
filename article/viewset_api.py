add2e0acbbbb1a3d42607f43dca1c422706cdf57
onyalevanaa <onyakamae12@smk.belajar.id>
2022-10-21 15:55:27 +0700

    article

--- article/viewset_api.py
+++ article/viewset_api.py
@@ -1,7 +1,11 @@
-from article.models import Kategori
-from article.serializers import KategoriSerializer
+from article.models import Kategori, Artikel
+from article.serializers import KategoriSerializer, ArtikelSerializer
 from rest_framework import viewsets
 
 class KategoriViewSet(viewsets.ModelViewSet):
     queryset = Kategori.objects.all()
     serializer_class = KategoriSerializer
+
+class ArtikelViewSet(viewsets.ModelViewSet):
+    queryset = Artikel.objects.all()
+    serializer_class = ArtikelSerializer
